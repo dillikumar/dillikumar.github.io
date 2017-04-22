bdfd4d243741e59f9839e2c545ffd3abb1859fe3
dlipdhunter <dlipdhunter@gmail.com>
2017-04-22 15:22:27 +0530

    Updated with new styles

---
+++ Application/app.js
@@ -0,0 +1,16 @@
+// App initialization
+angular.module("dlipdhunter.github.io", []);
+
+// Controller : Home
+angular.module("dlipdhunter.github.io").controller("HomeCtrl", ["$scope", "$http", fnHomeCtrl]);
+
+
+// fnHomeCtrl
+function fnHomeCtrl($scope, $http){
+    $scope.Repos = [];
+    $http.get('Contents/repos.json')
+    .then(function(res){
+        console.log(res);
+        $scope.Repos = res.data;
+    });
+}
--- javascripts/controllers.js
+++
@@ -1,7 +0,0 @@
-angular.module('dillikumar.github.io').
-controller('aboutCtrl', ['$scope', function ($scope) {
-    console.log("About");
-}]).
-controller('homeCtrl', ['$scope', function ($scope) {
-    console.log("Home");
-}])
